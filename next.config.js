4620742b5a543ab28cc32d1044c3d0cffbb05bcd
kazu-pl <91014542+kazu-pl@users.noreply.github.com>
2021-12-11 16:28:22 +0100

    SVG as ReactComponent by configuring next.config.js (#2)

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      // issuer: {
+      // test: /\.(js|ts)x?$/,
+      // },
+      use: ["@svgr/webpack", "url-loader"],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
