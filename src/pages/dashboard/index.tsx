595a75f18a693fa296196f6a7853eee439702508
Kamil Żurad <kamil.zurad@gmail.com>
2022-09-14 14:13:47 +0200

    fix mobile styles

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,16 +2,21 @@ import type { NextPage } from "next";
 import HeadDecorator from "components/HeadDecorator";
 import PrivateRoute from "common/router/PrivateRoute";
 import DashboardWrapper from "common/wrappers/DashboardWrapper";
-import Box from "components/Box";
+import { useAppSelector } from "common/store/hooks";
+import { selectUserProfile } from "core/store/userSlice";
+import { Typography } from "antd";
 
 const DashboardPage: NextPage = () => {
+  const userData = useAppSelector(selectUserProfile);
   return (
     <>
       <HeadDecorator title="dashbaord" description="Strona dashbaordu" />
 
       <PrivateRoute>
         <DashboardWrapper title="Dashboard">
-          <Box>Hello!</Box>
+          <Typography.Title level={5} style={{ fontWeight: "normal" }}>
+            Hello {userData?.name} {userData?.surname}!
+          </Typography.Title>
         </DashboardWrapper>
       </PrivateRoute>
     </>
